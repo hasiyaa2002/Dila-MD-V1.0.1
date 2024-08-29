655ae9e15ed44086c1a698edd6b1000baabd4821
hasiyaa2002 <156837724+hasiyaa2002@users.noreply.github.com>
2024-08-29 19:01:23 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "aN8GzJRR#TlAddL1CUl8F376x1kU3NeOSyWWBNbxc5rZxpywd2oY",
+SESSION_ID: process.env.SESSION_ID || "SbIGTDDR#t-zmLdUQo_KkXBRSrPhhYyy1DvXcjUJCfigIKdJyJMI",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 };
